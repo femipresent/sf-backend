30a7ff275a972d0170def29f0b939c227a082d1a
femiclassic11-ai <femiclassic11@gmail.com>
2026-03-18 20:14:49 +0100

    update

--- node-http-server/src/routes/userProfile.router.js
+++ node-http-server/src/routes/userProfile.router.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { getUserProfile } = require("../user/user.controller");
-const authMiddleware = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authMiddleware");
 
 const userProfileRouter = express.Router();
 
-userProfileRouter.use(authMiddleware);
+userProfileRouter.use(protect);
 userProfileRouter.route("/profile").get(getUserProfile);
 
 module.exports = userProfileRouter;
